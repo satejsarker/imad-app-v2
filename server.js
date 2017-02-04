92a8fbaf323e4bc8c8ae96eadb771c57a1a9e70c
satejsarker <satejall@gmail.com>
2017-02-05 01:01:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/satej-profile',function(req,res)
+app.get('/about/',function(req,res)
 {
-    res.send("satej's profile will be avilable very soon ");
+    res.sendfile(path.join(_dirname,'ui','about.html'));
 })
 
 app.get('/ui/style.css', function (req, res) {
