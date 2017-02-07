86f35da95a676f4744208240fe44b9b6467cfb6c
satejsarker <satejall@gmail.com>
2017-02-08 01:39:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 console.log('Loaded!');
 var text=document.getElementById("text");
 var d=new Date();
-text.innerHTML= ((d.getDate()+1)+"/"+(d.getMonth()+1)+"/"+d.getFullYear());
 
 
 var img= document.getElementById('madi');
