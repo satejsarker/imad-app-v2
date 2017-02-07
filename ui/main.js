34f72acedccc28256b9898b0224e79f19b72f4eb
satejsarker <satejall@gmail.com>
2017-02-07 23:23:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,6 @@ text.innerHTML="value of me ";
 var img= document.getElementById('madi');
 img.onclick=function()
 {
-    img.style.marginLeft='200px';
+    var interval=setInterval(moveRight,100)
+ 
 };
