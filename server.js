67f5ce641c79843addbb4c466197be19aea8b572
satejsarker <satejall@gmail.com>
2017-02-16 00:51:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/ui/style.css', function (req, res) {
 var counter=0;
 app.get('/counter', function(req, res)
 {
-   counter=counter + 1;
+   counter+=1;
    res.send(counter.toString());
 });
 
